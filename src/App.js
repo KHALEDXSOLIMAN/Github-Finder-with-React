f25cfe73b8713deb2a3dabacc6c1580ec124cc84
khaledSoliman <khaledx.lap@gmail.com>
2020-05-04 23:58:37 +0200

    get users from github api l12

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/Users/Users';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>
     );
   }
