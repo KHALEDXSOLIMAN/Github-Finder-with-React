b47539d9324fbd0fcdceb81006a22b99ab06a119
khaledSoliman <khaledx.lap@gmail.com>
2020-05-15 04:18:58 +0200

    L20. Single User Component  Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/Users/Users';
+import User from './components/Users/User';
 import Search from './components/Users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -13,6 +14,7 @@ import './App.css';
 
 class App extends Component {
   state = {
+    user: {},
     users: [],
     loading: false,
     alert: null,
@@ -30,6 +32,14 @@ class App extends Component {
     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false });
   };
+  //get single github user
+  getUser = async (login) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${login}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ user: res.data, loading: false });
+  };
+
   //clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -42,7 +52,7 @@ class App extends Component {
     }, 4000);
   };
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -77,6 +87,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" Component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>{' '}
         </div>
--- src/components/Users/Usersitem.js
+++ src/components/Users/Usersitem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Usersitem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -12,9 +13,9 @@ const Usersitem = ({ user: { login, avatar_url, html_url } }) => {
         ></img>
         <h3>{login}</h3>
         <div>
-          <a className="btn btn-dark btn-sm my-1" href={html_url}>
+          <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
             More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
