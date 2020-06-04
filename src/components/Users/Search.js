7e7c10bf5b2376ce12cba703eea7c5ee4310813c
khaledSoliman <khaledx.lap@gmail.com>
2020-06-04 11:57:30 +0200

    ** L23. Search Class to Function With useState Hook

--- src/components/Users/Search.js
+++ src/components/Users/Search.js
@@ -1,63 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
-  state = { text: '' };
-  PropTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
+const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+  const [text, setText] = useState('');
 
-  onCange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onCange = (e) => {
+    setText(e.target.value);
   };
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('please enter something', 'secondary ');
+    if (text === '') {
+      setAlert('please enter something', 'secondary ');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText('');
     }
   };
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <div className="form-group">
-            <input
-              className="rounded mb row-contant"
-              type="text"
-              name="text"
-              value={this.state.text}
-              onChange={this.onCange}
-              placeholder="Search Users"
-            />
-          </div>
 
-          <div className="form-group">
-            {' '}
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-dark btn-block rounded  row-contant mt"
-            />
-          </div>
-        </form>
-        {showClear && (
-          <button
-            className="btn btn-light btn-block rounded"
-            onClick={clearUsers}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <div className="form-group">
+          <input
+            className="rounded mb row-contant"
+            type="text"
+            name="text"
+            value={text}
+            onChange={onCange}
+            placeholder="Search Users"
+          />
+        </div>
 
+        <div className="form-group">
+          {' '}
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block rounded  row-contant mt"
+          />
+        </div>
+      </form>
+      {showClear && (
+        <button
+          className="btn btn-light btn-block rounded"
+          onClick={clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 export default Search;
