eba90f3617866dd88a888a072f64a58183da1bd5
khaledSoliman <khaledx.lap@gmail.com>
2020-06-13 16:26:37 +0200

    L31. Home  NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/Users/Users';
 import User from './components/Users/User';
-import Search from './components/Users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import ALertState from './context/alert/AlertState';
 
@@ -23,28 +23,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-
-                      <Users />
-                    </Fragment>
-                  )}
-                />
-                <Route
-                  exact
-                  path="/about"
-                  render={(props) => (
-                    <Fragment>
-                      <About></About>
-                    </Fragment>
-                  )}
-                />
-                <Route exact path="/about" Component={About} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>{' '}
           </div>
